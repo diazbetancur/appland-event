649b9fbf9fe702b5fd175b2894ad6c982c14da00
John Alexander Diaz Betancur <johdiaz@nequi.com>
2025-02-04 20:33:50 -0500

    update data

--- src/app/features/componets/info/info.config.ts
+++ src/app/features/componets/info/info.config.ts
@@ -4,5 +4,7 @@ export const InfoConfig = Object.freeze({
     buyInfo: 'COMPRA TU ENTRADA',
     infoData: `No te quedes fuera y asegura tu lugar ahora mismo. Adquiere tu entrada y forma parte de esta experiencia única llena de aprendizaje, networking y momentos inolvidables. ¡Los cupos son limitados!`,
     buttonText: 'COMPRA TU ENTRADA AHORA',
+    urlForm:
+      'https://forms.office.com/pages/responsepage.aspx?id=1rNJlrW0v0mhIjqLsLeq8Rzqnoe_6qBHnlZCdFTbPFJURFI1VDk5RUZBNUhQMkJSTTAzSjdLQkxRUy4u&origin=QRCode&qrcodeorigin=presentation&route=shorturl',
   },
 });
